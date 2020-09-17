64992d0c11ebb707b7f4181cdde19cdc3100d74e
CocoaPebble <jyz18221816395@gmail.com>
2020-09-16 23:40:48 -0400

    Updates

---
+++ precache-manifest.59f97c5cd2ef481186dad2b43fd1264b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30605c63fbaa212afc6a3e97429c011c",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "5465590ba234fa688ee8",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f8d0e2a94fd92403f579",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5465590ba234fa688ee8",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/js/2.ebe1615d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/js/2.ebe1615d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d0e2a94fd92403f579",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/js/main.5e0c5d4c.chunk.js"
+  },
+  {
+    "revision": "ae06a656c2096c7fc07a",
+    "url": "/Yingzhou-Jiang-Shopping-Cart/static/js/runtime-main.4db0f4a5.js"
+  }
+]);
